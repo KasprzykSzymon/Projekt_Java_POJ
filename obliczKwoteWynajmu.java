c222bca2a25fc783e19b906c327a7e2fa4d62884
Szymon Kasprzyk <94121486+KasprzykSzymon@users.noreply.github.com>
2023-04-07 21:32:39 +0200

    (obliczKwoteWynajmu) Szybkie poprawki (Nie kompilowane)

--- obliczKwoteWynajmu.java
+++ obliczKwoteWynajmu.java
@@ -7,50 +7,67 @@ public class obliczKwoteWynajmu implements ActionListener {
     MyFrame myFrame = new MyFrame(); //creates a myFrame
     JTextField textField;
     JButton buttonOblicz;
+    JButton buttonPowrot;
 
 
         public obliczKwoteWynajmu(){
 
-            String obecnySamochod = "";
+//             String obecnySamochod = "";
             JLabel label = new JLabel("Oblicz kwote wynajmu: ");
-            
             
             JPanel panelStart =new JPanel();
             panelStart.setBackground(new Color(0,200,0));
           
             panelStart.add(label, BorderLayout.CENTER);
             
-            myFrameStart.add(panelStart, BorderLayout.NORTH);
+            myFrame.add(panelStart, BorderLayout.NORTH);
 
             JPanel panel = new JPanel();
-          
-            buttonOblicz = new JButton("Oblicz");
-            panel.add(buttonOblicz);
-            buttonOblicz.addActionListener(this);
-            buttonOblicz.setBorder(BorderFactory.createEtchedBorder());
-            buttonOblicz.setBackground(new Color(50,120,200));
-          
-            textField = new JTextField();
-            texfField.background(Color.BLACK);
-            panel.add(textField);
-          
-            myFrame.add(panel, BorderLayout.CENTER);
             panel.setBackground(new Color(0,200,0));
             panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
             panel.setLayout(new GridLayout(0,2,10,5));
+          
+            buttonOblicz = new JButton("Oblicz");
             
-
+            buttonOblicz.addActionListener(this);
+            buttonOblicz.setBorder(BorderFactory.createEtchedBorder());
+            buttonOblicz.setBackground(new Color(50,120,200));
+            panel.add(buttonOblicz);
             
+            textField = new JTextField();
+            textField.setPreferredSize(new Dimension(250,40));
+            textField.setFont(new Font("Arctic", Font.PLAIN,35));
+            textField.setForeground(Color.GREEN);
+            textField.setBackground(Color.BLACK);
+            textField.setCaretColor(Color.WHITE);
+            panel.add(textField);
+         
+            myFrame.add(panel, BorderLayout.CENTER);
+            
+            
+            JPanel panel2 = new JPanel();
+            panel2.setBackground(new Color(0,200,0));
+            panel2.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+            panel2.setLayout(new GridLayout(0,4,10,5));
+            
+            myFrame.add(panel2, BorderLayout.SOUTH);
 
+            buttonPowrot = new JButton(" Powrot ");
+            buttonPowrot.addActionListener(this);
+            buttonPowrot.setBackground(new Color(255,100,100));
+            buttonPowrot.setBorder(BorderFactory.createEtchedBorder());
+            buttonPowrot.setSize(250,20);
+            panel2.add(buttonPowrot);
 
+        
         }
-        }
+
         @Override
         public void actionPerformed(ActionEvent e){
-            if(e.getSource()==buttonDodajSamochod){
-                myFrameStart.dispose();
-                DodajSamochod dodajSamochod = new DodajSamochod();
+            if(e.getSource()==buttonPowrot){
+                myFrame.dispose();
+                GUI gui = new GUI();
             }
-
+        
         }
 }
